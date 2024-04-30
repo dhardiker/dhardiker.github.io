25f4190f7814ba8532d740c3d20703a060f307be
Dan Hardiker <dan@hardiker.co.uk>
2024-04-30 17:26:29 +0100

    Rebuilding the Arcade Leaderboard in a new monitor friendly template

--- src/routes/Leaderboard.tsx
+++ src/routes/Leaderboard.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Container from '@mui/material/Container'
 import Box from '@mui/material/Box'
-import Button from '@mui/material/Button'
 import List from '@mui/material/List'
 import ListItem from '@mui/material/ListItem'
 import ListItemText from '@mui/material/ListItemText'
@@ -11,7 +10,7 @@ import Typography from '@mui/material/Typography'
 import { Helmet } from "react-helmet-async"
 import { useNavigate } from "react-router-dom"
 import { apiClientHooks } from '../bootstrapping/InitApiClient'
-import { Skeleton } from '@mui/material'
+import { Paper, Skeleton } from '@mui/material'
 
 const SkeletonListItem: React.FC = () => (
   <ListItem alignItems="flex-start">
@@ -29,18 +28,29 @@ type ScoreListItemProps = {
 }
 const ScoreListItem: React.FC<ScoreListItemProps> = ({ name, score, gamesPlayed }) =>
   <ListItem alignItems="flex-start">
-    <ListItemAvatar>
-      <Avatar name={name} round={true} size='42px' />
+    <ListItemAvatar sx={{ marginRight: 2 }}>
+      <Avatar name={name} round={true} size='6vh' />
     </ListItemAvatar>
     <ListItemText
-      primary={`${Intl.NumberFormat("en-GB", { maximumFractionDigits: 0 }).format(score)} points`}
+      // sx={{ color: 'white', fontSize: '5rem' }}
+      primary={
+        <Typography
+          sx={{ display: 'inline' }}
+          component="span"
+          fontSize='2.5vh'
+          color="white"
+        >
+          {Intl.NumberFormat("en-GB", { maximumFractionDigits: 0 }).format(score)} points
+        </Typography>
+      }
       secondary={
         <React.Fragment>
           <Typography
-            sx={{ display: 'inline' }}
+            sx={{ display: 'block', overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap' }}
             component="span"
             variant="body2"
-            color="text.primary"
+            fontSize='1.5vh'
+            color="lightgray"
           >
             {name}
           </Typography>
@@ -50,48 +60,104 @@ const ScoreListItem: React.FC<ScoreListItemProps> = ({ name, score, gamesPlayed 
     />
   </ListItem>
 
+const ensureArrayHasExactLength = <T extends unknown>(arr: T[] | undefined, length: number, fill: T): T[] => {
+  if (arr === undefined) return Array(length).fill(fill)
+  if (arr.length === length) return arr
+  if (arr.length > length) return arr.slice(0, length)
+  return [...arr, ...Array(length - arr.length).fill(fill)]
+}
+
 export const Element: React.FC = () => {
   const navigate = useNavigate()
 
   const leaderboard = apiClientHooks.useGetPublicLeaderboard()
   if (process.env.NODE_ENV === 'development') console.log('leaderboard:', leaderboard)
 
+  const scoreCount = 5
+  const highscores = ensureArrayHasExactLength(leaderboard.data?.highscores, scoreCount, { name: 'Realsdoginoinsodign Long----', highscore: 0, rank: 0 })
+
   return <>
     <Helmet>
       <title>Leaderboard</title>
     </Helmet>
-    <Container component="main">
-      <Box
-        sx={{
-          marginTop: 8,
+    <Container component="main" maxWidth={false} disableGutters>
+      <Box sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '100vh',
+        width: '100vw',
+        bgcolor: 'black',
+        color: 'white',
+      }}>
+        <img src="/arcade-title.png" alt="Arcade Title" style={{ position: 'absolute', top: 0, left: -25, width: '65vw' }} />
+        <img src="/logo-devoxx-white.png" alt="Devoxx" style={{ position: 'absolute', bottom: 60, left: 20, width: '25vw' }} />
+        <img src="/arcade-heads.png" alt="Arcade Heads" style={{ position: 'absolute', bottom: -20, right: 20, width: '25vw' }} />
+        <Box sx={{
           display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-        }}
-      >
-        <Typography component="h1" variant="h5" marginBottom={3}>
-          Leaderboard
-        </Typography>
-        {!leaderboard.isLoading && leaderboard.data?.highscores.length === 0 && (
-          <Typography component="h2" variant="h2" marginBottom={3}>
-            <em>There are no scores yet!</em>
-          </Typography>
-        )}
-        <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
-          {leaderboard.isLoading && <SkeletonListItem />}
-          {leaderboard.data?.highscores.map((entry, index) => (
-            // TODO: Add a player key to the leaderboard API
-            // TODO: Add gamesPlayed to the leaderboard API
-            <ScoreListItem key={index} name={entry.name} score={entry.highscore} />
-          ))}
-        </List>
-        <Button
-          variant="contained"
-          sx={{ marginTop: 3 }}
-          onClick={() => navigate('/')}
-        >
-          Back
-        </Button>
+          justifyContent: 'space-around',
+          width: '80%',
+          maxWidth: '80vh'
+        }}>
+          <Box sx={{ position: 'relative', flex: 1, m: 1, marginTop: 8, marginRight: 3 }}>
+            <Typography variant="h4" sx={{
+              fontWeight: 'bold',
+              color: 'white',
+              position: 'absolute',
+              top: '-4.5vh', // Adjust this value as needed
+              left: '1vh', // Adjust for alignment
+              zIndex: 'tooltip'
+            }}>
+              Overall Standings
+            </Typography>
+            <Paper elevation={6} sx={{
+              bgcolor: 'black',
+              height: `${9 * scoreCount}vh`, // Adjust as per content
+              border: '0.25vh solid limegreen',
+              borderRadius: '2vh',
+              display: 'flex',
+              alignItems: 'top',
+              justifyContent: 'center',
+              width: '45vh',
+            }}>
+
+              <List sx={{ width: '100%', maxWidth: '35vh' }}>
+                {leaderboard.isLoading && <SkeletonListItem />}
+                {highscores.map((entry, index) => (
+                  // TODO: Add a player key to the leaderboard API
+                  // TODO: Add gamesPlayed to the leaderboard API
+                  <ScoreListItem key={index} name={entry.name} score={entry.highscore} />
+                ))}
+              </List>
+
+            </Paper>
+          </Box>
+          <Box sx={{ position: 'relative', flex: 1, m: 1, marginTop: 8 }}>
+            <Typography variant="h4" sx={{
+              fontWeight: 'bold',
+              color: 'white',
+              position: 'absolute',
+              top: '-4.5vh', // Adjust this value as needed
+              left: '1vh', // Adjust for alignment
+              zIndex: 'tooltip'
+            }}>
+              Game Name
+            </Typography>
+            <Paper elevation={6} sx={{
+              bgcolor: 'black',
+              height: `${9 * scoreCount}vh`, // Adjust as per content
+              border: '0.25vh solid limegreen',
+              borderRadius: '2vh',
+              display: 'flex',
+              alignItems: 'top',
+              justifyContent: 'center',
+              width: '45vh',
+            }}>
+              {/* Replace with dynamic content */}
+            </Paper>
+          </Box>
+        </Box>
       </Box>
     </Container>
   </>
