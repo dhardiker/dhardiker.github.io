c1ca996c68b41d4a7754b5472970f80084418d78
Dan Hardiker <dan@hardiker.co.uk>
2024-05-07 20:37:00 +0100

    Removing devtools locally

--- src/components/Devtools.tsx
+++ src/components/Devtools.tsx
@@ -3,7 +3,7 @@ import { useLocation } from "react-router-dom"
 
 const Devtools: React.FC = () => {
   const location = useLocation()
-  const showDevTools = process.env.NODE_ENV === 'development' || new URLSearchParams(location.search).has('devtools')
+  const showDevTools = new URLSearchParams(location.search).has('devtools')
   if (!showDevTools) return null
 
   return <ReactQueryDevtools initialIsOpen={false} />
